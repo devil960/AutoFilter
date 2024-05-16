8ed273180454926ad3ce509cc50574532c9394af
devil960 <133393089+devil960@users.noreply.github.com>
2024-05-16 16:57:57 +0530

    Update url_shortner.py

--- plugins/modules/url_shortner.py
+++ plugins/modules/url_shortner.py
@@ -15,7 +15,7 @@ OWLY_API = os.environ.get("OWLY_API", None)
 BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text='⚙ Join Updates Channel ⚙', url='https://telegram.me/team_netflix')
+            InlineKeyboardButton(text='⚙ Updates Channel ⚙', url='https://telegram.me/MW_BOTS')
         ]
     ]
 )
