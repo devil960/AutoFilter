66d9d0f1d949fca892a9a435417eba25993f2db1
devil960 <133393089+devil960@users.noreply.github.com>
2024-05-17 12:26:43 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -102,7 +102,7 @@ async def fil_mod(client, message):
         await m.edit("ᴜsᴇ :- /autofilter on ᴏʀ /autofilter off")
 
 
-@Client.on_message((filters.group) & filters.text & filters.incoming)
+@Client.on_message((filters.group) & filters.private & filters.text & filters.incoming)
 async def give_filter(client, message):
     await global_filters(client, message)
     group_id = message.chat.id
